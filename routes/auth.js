b971f7ee8c764f379d7332acfde46dabb84e0007
jasir-pv <jasirahsanpv40@gmail.com>
2024-09-26 21:40:40 +0530

    Mongodb Authentication Completed and jwt intalled accestoken working

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 import express, { Router } from "express"
-import User from "../models/User"
+import User from "../models/User.js"
+import CryptoJS from "crypto-js"
+import jwt from "jsonwebtoken"
 
 
 const router = Router()
@@ -9,10 +11,12 @@ const router = Router()
 
 router.post("/register",async (req,res)=>{
 
-    const newUser = newUser({
+    const newUser = new User({
             username:req.body.username,
             email:req.body.email,
-            password:req.body.password,
+            password:CryptoJS.AES.encrypt(
+                req.body.password, 
+                process.env.PASS_SEC).toString()
 
     })
     try{
@@ -27,6 +31,36 @@ router.post("/register",async (req,res)=>{
     
 })
 
+
+// LOGIN
+
+    router.post("/login", async( req,res)=>{
+        try{
+            const user= await User.findOne({username:req.body.username})
+            !user && res.status(401).json("Wrong credantials")
+            const hashedPassword = CryptoJS.AES.decrypt(
+                user.password,
+                process.env.PASS_SEC
+            )
+
+            const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8)
+
+            originalPassword!== req.body.password &&
+            res.status(401).json("wrong credentials")
+
+            const accessToken = jwt.sign({
+                id:user._id, isAdmin: user.isAdmin
+            },process.env.JWT_SEC,
+        {expiresIn:"3d"})
+
+            const {password, ...others} = user._doc
+
+            res.status(200).json({others,accessToken})
+        } catch (err){
+            res.status(500).json(err)
+        }
+
+    })
 
 
 export default router;
--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ import express, { Router } from "express"
 
 const router = Router()
 
-
+router.put("/:id",)
 
 
 export default router;
